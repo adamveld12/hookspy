b939923968bdb0369a25a66877eef83c21260c92
Adam Veldhousen <adamveld12@gmail.com>
2016-11-20 22:21:36 -0600

    added hyper compose file

--- hyper-compose.sh
+++ hyper-compose.sh
@@ -1,15 +1,27 @@
 version: '2'
 services:
-  api:
+  web:
+    size: S1
     environment:
-      HOOKSPY_DEBUG: false
+      - "HOOKSPY_DEBUG=false"
     image: adamveld12/hookspy
-    fip: 209.177.93.123
     links:
       - db:db
     depends_on:
       - db
-    ports:
-      - "80:80"
   db:
+    size: S2
     image: rethinkdb
+    volumes:
+      - dbstorage:/data
+  lb:
+    size: S2
+    image: 'dockercloud/haproxy:latest'
+    fip: 209.177.93.123
+    links:
+      - web
+    depends_on:
+      - web
+    ports:
+      - '80:80'
+      - '443:443'
