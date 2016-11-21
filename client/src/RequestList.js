38ac5005b132fd33d236b2f0bfe7e5d5711ff505
Adam Veldhousen <adamveld12@gmail.com>
2016-11-20 22:50:12 -0600

    added instruction test for new hook pages

--- client/src/RequestList.js
+++ client/src/RequestList.js
@@ -8,7 +8,10 @@ export default class RequestList extends Component {
     const { requests } = this.props;
     return (
         <ul className="RequestList">
-          { (requests || []).length <= 0 ? (<li> Requests will show up here </li>) : "" }
+          { (requests || []).length <= 0 ? (<li>
+              <p>Send HTTP requests to the link at the top in the white box.</p>
+              <p>Requests will show up here, newest at the top.</p>
+             </li>) : "" }
           {
               (requests || []).map((r, idx) => (
                 <li key={idx}>
